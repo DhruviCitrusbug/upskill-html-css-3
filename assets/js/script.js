8e785fb2c832e5690e089a8e51cad15d9f6bb269
DhruviCitrusbug <dhruvi.citrusbug@gmail.com>
2025-02-10 19:30:44 +0530

    HTML-CSS design implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
-    console.log("JQuery ready");
+    
+    function handleSidebarCollapse() {
+        let sidebar = $("#detail-sidebar");
+        let iconSidebar = $("#icon-sidebar");
+    
+        if ($(window).width() <= 766) {
+            sidebar.addClass("hide-sidebar");
+            iconSidebar.addClass("show-sidebar");
+        } else {
+            sidebar.removeClass("hide-sidebar");
+            iconSidebar.removeClass("show-sidebar");
+        }
+    }
+
+    handleSidebarCollapse();
+
+    $(window).resize(function () {
+        handleSidebarCollapse();
+    });
+
     $("#hide-sidebar").click(function (e) {
-        console.log("clicked");
         e.preventDefault();
         let sidebar = $("#detail-sidebar");
         let iconSidebar = $("#icon-sidebar");
@@ -15,6 +33,14 @@ $(document).ready(function () {
         let iconSidebar = $("#icon-sidebar");
         sidebar.toggleClass("hide-sidebar");
         iconSidebar.toggleClass("show-sidebar");
+    });
+
+    $(".detail-sidebar-item").click(function (e) {
+        console.log("clicked");
+        e.preventDefault();
+        let subMenu = $(this).next(".detail-sidebar-sub-items");
+        $(this).toggleClass("more-info");
+        subMenu.toggleClass("more-info");
     });
 
 });
